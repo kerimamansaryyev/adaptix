a46b2691f857eb5f397aa39e9b86f30a227fb219
kerimthegreat <keamansaryyev@example.com>
2021-10-28 23:53:49 +0500

    Made scaleFactors more customizable

--- lib/adaptix.dart
+++ lib/adaptix.dart
@@ -1,14 +1,24 @@
 library adaptix;
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'dart:io';
 export 'package:adaptix/adaptix.dart';
 part 'package:adaptix/src/models/sizing.dart';
 part 'package:adaptix/src/models/resizer.dart';
 part 'package:adaptix/src/widgets/initializer.dart';
 
-EdgeInsets get systemPadding => _Sizing.systemPadding;
-double get systemAspectRatio => _Sizing.aspectRatio;
+
+abstract class Adaptix{
+  static EdgeInsets get systemPadding => _Sizing.systemPadding;
+  static double get systemAspectRatio => _Sizing.aspectRatio;
+
+  static double xSmallScreenScaleFactor = 1.0;
+  static double smallScreenScaleFactor = 1.1;
+  static double mediumScreenScaleFactor = 1.15;
+  static double largeScreenScaleFactor = 1.2;
+  static double xLargeScreenScaleFactor = 1.25;
+  static double commonScaleFactor = 1.0;
+
+}
 
 extension SizingExtendsion on num{
 
@@ -16,14 +26,13 @@ extension SizingExtendsion on num{
 
   double get orientationHeight => _Sizing._blockSizeVertical*this;
 
-  double adaptedPx({bool useIphoneScaleFactor = true}){
-    var isIphoneSizeFactor = Platform.isIOS && useIphoneScaleFactor? 1.12: 1.0;
+  double adaptedPx(){
     return this*Resizer<double>(
-      xSmall: 1*isIphoneSizeFactor,
-      small: 1.1*isIphoneSizeFactor, 
-      medium: 1.15*isIphoneSizeFactor, 
-      large: 1.2*isIphoneSizeFactor, 
-      xlarge: 1.25*isIphoneSizeFactor,
+      xSmall:Adaptix.xSmallScreenScaleFactor*Adaptix.commonScaleFactor,
+      small:Adaptix.smallScreenScaleFactor*Adaptix.commonScaleFactor, 
+      medium:Adaptix.mediumScreenScaleFactor*Adaptix.commonScaleFactor, 
+      large:Adaptix.largeScreenScaleFactor*Adaptix.commonScaleFactor, 
+      xlarge:Adaptix.xLargeScreenScaleFactor*Adaptix.commonScaleFactor,
       totalWidth: _Sizing._screenWidth
     ).value;
   }
